9a66e0a70211e847c2be0451a280197893ace0fe
Adufifi <Adufifi321@gmail.com>
2024-10-19 16:02:14 +0700

    add boostrap

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,12 @@
+
 @extends('layouts.app')
 @section('content')
-    <h1>Dashboard</h1>
+<div class="card" style="width: 18rem;">
+    <img src="https://cdn.iha.com.tr/Contents/images/2019/35/3021942.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/dashboard', [UserController::class, 'dashboard']);
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
